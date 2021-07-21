b6258725716d8dcac3b97d842aa7a04524692477
Shikhar Adhikari <67640904+Bibhusan7@users.noreply.github.com>
2021-07-21 19:43:54 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from instapy import InstaPy
 from instapy import smart_run
-your_password = ""
-session = InstaPy(username="botted7777", password=your_password, headless_browser=False)
+session = InstaPy(username="yourUsername", password="yourPassword", headless_browser=False)
 with smart_run(session):
     session.set_relationship_bounds(enabled=True, delimit_by_numbers=True, max_followers=5000, min_following=10, min_followers=10)
     session.follow_user_followers(['cristiano'], amount=10, randomize=False)
